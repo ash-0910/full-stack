b5274b07d14e15aaa671fa5a1ab4981a4201d633
ashish <ashishsurti0910@gmail.com>
2025-11-07 15:31:26 -0500

    add frontend prod url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState("")
 
   useEffect(() => {
-    fetch("http://localhost:4000/api/message")
+    fetch("http://72.61.6.58:4000/api/message")
     .then((res)=> res.json())
     .then((data)=> setMessage(data.message))
     .catch((err) => {
