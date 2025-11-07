c6b9d9dcfff0f749b1a9510aa0962e6596c6cf10
ashish <ashishsurti0910@gmail.com>
2025-11-07 14:53:08 -0500

    first push to production

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/message")
+    .then((res)=> res.json())
+    .then((data)=> setMessage(data.message))
+    .catch((err) => {
+      console.error("error fetching message", err)
+    })
+  }, [])
+
+  return (
+    <>
+
+    <h1>Welcome to Ashish's frontend</h1>
+      <h2>
+        data {message}
+      </h2>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+
+app.use(express.json())
+app.use(cors(
+    {
+         origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:3000",
+            // add production url 
+        ],
+        credentials: true , 
+    
+    }
+   
+))
+
+
+// API route
+
+app.get("/api/message" , (req, res) => {
+    res.json({message: "Hello , from Ashish's server"});
+});
+
+
+const PORT = 4000
+
+app.listen(PORT,"0.0.0.0", () => console.log(`server is running at http://localhost:${PORT}`))
